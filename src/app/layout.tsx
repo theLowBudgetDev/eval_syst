d42eb6b232a3a9c57aeb894c73be92b60e7952d0
Enrich Ugboji Onah <onahenrich@gmail.com>
2025-06-23 16:07:14 +0000

    can you create a favicon, with the system logo icon and add to the tab h

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
